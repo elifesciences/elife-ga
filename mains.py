47e3edfa01552347f45708e67b585ad9a8fabcd6
Luke Skibinski <l.skibinski@elifesciences.org>
2015-09-10 12:24:01 +0100

    added pdf download counts

--- mains.py
+++ mains.py
@@ -25,9 +25,31 @@ argparser.add_argument('table_id', type=str,
 def ymd(dt):
     return dt.strftime("%Y-%m-%d")
 
-def path_counts(service, table_id, from_date, to_date):
-    "talks to GA and returns a list of pairs of [path, count]"
+def event_counts(service, table_id, from_date, to_date):
+    "returns the raw GA results for PDF downloads between the two given dates"
+    assert isinstance(from_date, datetime), "'from' date must be a datetime object. received %r" % from_date
+    assert isinstance(to_date, datetime), "'to' date must be a datetime object. received %r" % to_date
     
+    return service.data().ga().get(
+        ids = table_id,
+        max_results=10000, # 10,000 is the max GA will ever return
+        start_date = ymd(from_date),
+        end_date = ymd(to_date),
+        metrics = 'ga:totalEvents',
+        dimensions = 'ga:eventLabel',
+        sort = '-ga:totalEvents',
+        # ';' separates AND expressions, ',' separates OR expressions
+        filters = r'ga:eventAction==Download;ga:eventCategory==Article;ga:eventLabel=~pdf-article',
+    )
+
+def download_counts(row_list):
+    def parse(row):
+        label, count = row
+        return label.split('::')[0], count
+    return dict(map(parse, row_list))
+
+def path_counts(service, table_id, from_date, to_date):
+    "returns the raw GA results for article page views between the two given dates"
     assert isinstance(from_date, datetime), "'from' date must be a datetime object. received %r" % from_date
     assert isinstance(to_date, datetime), "'to' date must be a datetime object. received %r" % to_date
 
@@ -47,6 +69,7 @@ def path_counts(service, table_id, from_date, to_date):
     
     return service.data().ga().get(
         ids = table_id,
+        max_results=10000, # 10,000 is the max GA will ever return
         start_date = ymd(from_date),
         end_date = ymd(to_date),
         metrics = 'ga:pageviews',
@@ -73,14 +96,20 @@ def article_counts(path_count_pairs):
 
     def article_count(pair):
         "figures out the type of the given path using the suffix (if one available)"
-        bits = pair[0].split('/', 3)
-        art = bits[-1]
-        more_bits = re.split(splitter, art, maxsplit=1)
-        suffix = None
-        if len(more_bits) > 1:
-            art, suffix = more_bits
-        assert suffix in type_map, "unknown suffix %r! received: %r" % (suffix, pair)
-        return art, type_map[suffix], int(pair[1])
+        try:
+            bits = pair[0].split('/', 3)
+            art = bits[-1]
+            more_bits = re.split(splitter, art, maxsplit=1)
+            suffix = None
+            if len(more_bits) > 1:
+                art, suffix = more_bits
+            assert suffix in type_map, "unknown suffix %r! received: %r split to %r" % (suffix, pair, more_bits)
+            return art, type_map[suffix], int(pair[1])
+        except AssertionError:
+            # we have a weird path
+            print 'bar row:',pair
+            return []
+            
 
     # for each path, build a list of path_type: value
     article_groups = {}
@@ -90,7 +119,10 @@ def article_counts(path_count_pairs):
             'abstract': 0,
             'digest': 0,
         })
-        art, art_type, count = article_count(pair)
+        triplet = article_count(pair)
+        if not len(triplet) == 3:
+            continue # bad row
+        art, art_type, count = triplet
         group = article_groups.get(art, [row])
         group.append(Counter({art_type: count}))
         article_groups[art] = group
@@ -103,29 +135,11 @@ def article_counts(path_count_pairs):
     return {art: reduce(update, group) for art, group in article_groups.items()}    
 
 
-
-#
-# bootstrap
-#
-
-def main(argv):
-    """
-    has to be called with the 'table-id', which looks like 12345678
-    call this app like: python mains.py 'ga:12345678'"""
-    
-    # Authenticate and construct service.
-    service, flags = sample_tools.init(
-      argv, 'analytics', 'v3', __doc__, __file__, parents=[argparser],
-      scope='https://www.googleapis.com/auth/analytics.readonly')
-
-    today = datetime.now()
-    yesterday = today - timedelta(days=1)
-
+def query_ga(q):
     # Try to make a request to the API. Print the results or handle errors.
     try:
-        ga_results = path_counts(service, flags.table_id, yesterday, today).execute()
-        return article_counts(ga_results['rows'])
-    
+        return q.execute()
+            
     except TypeError, error:      
         # Handle errors in constructing a query.
         print ('There was an error in constructing your query : %s' % error)
@@ -141,7 +155,30 @@ def main(argv):
         # Handle Auth errors.
         print ('The credentials have been revoked or expired, please re-run '
                'the application to re-authorize')
-        raise
+        raise    
+
+
+#
+# bootstrap
+#
+
+def main(argv):
+    """has to be called with the 'table-id', which looks like 12345678
+    call this app like: python mains.py 'ga:12345678'"""
+    
+    # Authenticate and construct service.
+    service, flags = sample_tools.init(
+      argv, 'analytics', 'v3', __doc__, __file__, parents=[argparser],
+      scope='https://www.googleapis.com/auth/analytics.readonly')
+
+    today = datetime.now()
+    yesterday = today - timedelta(days=1)
+
+    view_results = query_ga(event_counts(service, flags.table_id, yesterday, today))
+    download_results = query_ga(path_counts(service, flags.table_id, yesterday, today))
+
+    return {'views': download_counts(view_results['rows']),
+            'downloads': article_counts(download_results['rows'])}
 
 if __name__ == '__main__':
     pprint(main(sys.argv))
