3c5d9864e81dab37c47d040ef5460772ccc1bda3
Luke Skibinski <l.skibinski@elifesciences.org>
2015-10-19 12:07:26 +0100

    replaced call to pylint2 for call to pylint

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source install.sh > /dev/null
-pylint2 -E elife_ga_metrics/*.py
+pylint -E elife_ga_metrics/*.py
 python -m unittest --verbose --failfast --catch elife_ga_metrics.test.tests
