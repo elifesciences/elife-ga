eadf89461175e818973f60756d811179fb683f3c
Luke Skibinski <l.skibinski@elifesciences.org>
2015-10-20 15:21:10 +0100

    added the tests to pylint checking

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 source install.sh > /dev/null
-pylint -E elife_ga_metrics/*.py
-#python -m unittest --verbose --failfast --catch elife_ga_metrics.test
+pylint -E elife_ga_metrics/*.py elife_ga_metrics/test/*.py
 python -m unittest discover --verbose --failfast --catch --start-directory elife_ga_metrics/test/ --pattern "*.py"
