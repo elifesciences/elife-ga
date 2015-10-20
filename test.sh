79bb011fdcb0c79c465f71a9ce4ce2be0bbc3426
Luke Skibinski <l.skibinski@elifesciences.org>
2015-10-20 11:59:41 +0100

    tweaked test script to detect tests better

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source install.sh > /dev/null
 pylint -E elife_ga_metrics/*.py
-python -m unittest --verbose --failfast --catch elife_ga_metrics.test.tests
+#python -m unittest --verbose --failfast --catch elife_ga_metrics.test
+python -m unittest discover --verbose --failfast --catch --start-directory elife_ga_metrics/test/ --pattern "*.py"
