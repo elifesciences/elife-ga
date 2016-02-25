c6416f099f8d06229fd17bae3a3a457644ef38b9
Luke Skibinski <l.skibinski@elifesciences.org>
2016-02-25 17:20:41 +0000

    updated test script so that everything must pass and that the contents of install.sh are not obscured

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-source install.sh > /dev/null
+set -e
+source install.sh
 pylint -E elife_ga_metrics/*.py elife_ga_metrics/test/*.py
 python -m unittest discover --verbose --failfast --catch --start-directory elife_ga_metrics/test/ --pattern "*.py"
