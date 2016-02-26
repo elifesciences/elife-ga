9535ad8f13ee883e3990f1a661a8aa313d2cac9c
Luke Skibinski <l.skibinski@elifesciences.org>
2016-02-26 17:13:20 +0000

    added coverage.py to testing. elife-ga-metrics is really poor :(

--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@ set -e
 source install.sh
 pylint -E elife_ga_metrics/*.py elife_ga_metrics/test/*.py \
     --ignored-classes=Resource # google-api-python-client.discovery.Resource - dynamicly set attributes
-python -m unittest discover --verbose --failfast --catch --start-directory elife_ga_metrics/test/ --pattern "*.py"
+coverage run --source='elife_ga_metrics/' -m unittest discover elife_ga_metrics/test/
+coverage report
