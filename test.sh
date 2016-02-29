54f8e08487f79543cca339d163704ef874b95e95
Luke Skibinski <l.skibinski@elifesciences.org>
2016-02-29 18:31:22 +0000

    fixed up the test runner, it wasn't including certain tests

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ set -e
 source install.sh
 pylint -E elife_ga_metrics/*.py elife_ga_metrics/test/*.py \
     --ignored-classes=Resource # google-api-python-client.discovery.Resource - dynamicly set attributes
-coverage run --source='elife_ga_metrics/' -m unittest discover elife_ga_metrics/test/
+coverage run --source='elife_ga_metrics/' -m unittest discover elife_ga_metrics.test -p '*_tests.py'
 coverage report
