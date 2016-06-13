c75de99f0cd2b7cfd777ed9e4a612d3f8f20ed51
Luke Skibinski <lsh@ccxx.cx>
2016-06-13 13:29:03 +0100

    added a minimum test coverage check after running tests

--- .test.sh
+++ .test.sh
@@ -5,4 +5,14 @@ if [ ! -z "$@" ]; then
     module=".$@"
 fi
 green --run-coverage -vv elife_ga_metrics.test"$module"
+
+# is only run if tests pass
+covered=$(coverage report | grep TOTAL | awk '{print $6}' | sed 's/\...%//')
+if [ $covered -le 65 ]; then
+    echo
+    echo "FAILED this project requires at least 65% coverage"
+    echo
+    exit 1
+fi
+
 coverage html
